cba52efab5d91a673f13a4621c18d8072b88c0a9
chji <chji@fortinet.com>
2024-10-02 11:00:17 -0700

    changing to hub.docker.com

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Open the wall, Mr JFK!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World! Welcome to my app."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
